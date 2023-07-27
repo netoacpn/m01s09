eedb285f85048d6fbe7f3e7e8e140a9207e5786c
Antonio Carlos Pereira Neto <133928083+netoacpn@users.noreply.github.com>
2023-07-27 20:16:15 -0300

    Add Ex03

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
 import './App.css'
 import AdicionarCard from './components/CardAdicionar'
 
 function App() {
+
+  const [listaTarefas, setListaTarefas] = useState([
+    {textoTarefa: "tarefa ", finalizado: false}    
+  ])
 
   return (
     <>
